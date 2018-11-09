9ff74513b3c177c3ec2bea6ece41a34c52b4a0e1
Sasha <sashakyunghwa@gmail.com>
2018-11-09 14:54:55 -0800

    login completed

--- main.js
+++ main.js
@@ -4,7 +4,47 @@ $(document).ready(function() {
     var close = document.getElementById('close');
 
 button.onclick = function() {
-    modal.style.display = 'block';
+    var name = $('.input-name').val();
+    var email = $('#email-input').val();
+    var nameValid = null;
+    var emailValid = null;
+    var atSymbol = '@'; 
+
+    if(name.length < 2 || name === '') {
+        $('.input-name').popover('show');
+        nameValid = false;
+        console.log('name is invalid');
+    } else {
+        $('.student-icon').popover('hide');
+        nameValid = true;
+        console.log('name is valid');
+    }
+
+    if(email === '') {
+        $('#email-input').popover('show');
+        emailValid = false;
+        console.log('email is really invalid');
+    }
+
+    for(var i = 0; i < email.length; i++) {
+        if(atSymbol.indexOf(email.charAt(i)) != -1 && email.length > 6) {
+            $('#email-input').popover('hide');
+            emailValid = true;
+            console.log('email is valid');
+        } else if(atSymbol.indexOf(email.charAt(i)) != -1 && email.length < 6) {
+            $('#email-input').popover('show');
+            emailValid = false;
+            console.log('email is invalid');
+        } else {
+            $('#email-input').popover('show');
+            emailValid = false;
+            console.log('email is hellllllla invalid');
+        }
+    } 
+  
+    if(nameValid == true && emailValid == true) {
+        modal.style.display = 'block';
+    }
 }
 
 close.onclick = function() {
@@ -16,5 +56,7 @@ window.onclick = function(e) {
         modal.style.display = "none";
     }
 }
+
 });
+
 
